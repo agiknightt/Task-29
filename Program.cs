d9e5daaa233fc9ca81bbe35489c86c1f4ff4ee3d
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-09-08 19:21:33 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(20, 10);
+            Renderer renderer = new Renderer();
+
+            renderer.Render(player1.PlayerY, player1.PlayerX);
+            Console.ReadKey();
+        }
+    }
+    class Player
+    {
+        public int PlayerX;
+        public int PlayerY;
+
+        public Player(int x, int y)
+        {
+            PlayerX = x;
+            PlayerY = y;
+        }        
+    }
+    class Renderer
+    {
+        public char SymbolPlayer = '@';
+
+        public void Render(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(SymbolPlayer);
+        }        
+    }
+}
